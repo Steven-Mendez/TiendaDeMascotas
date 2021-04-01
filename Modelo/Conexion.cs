99f73a5f3aded62c7e5fdae2a9710f7b2fe73972
Steven Mendez <stevenampaiz@gmail.com>
2021-04-01 17:59:42 -0600

    Corrección

--- Modelo/Conexion.cs
+++ Modelo/Conexion.cs
@@ -9,7 +9,7 @@ namespace Tienda_Mascota.Modelo
     class Conexion
     {
         private static readonly string dataSource = @"localhost";
-        private static readonly string initialCatalog = @"TiendaMascotas";
+        private static readonly string initialCatalog = @"MiEmpresa";
         private static readonly string user = @"ProductoMascotas";
         private static readonly string password = @"123";
         public static readonly string Cn = $"Data Source = {dataSource}; Initial Catalog = {initialCatalog}; user = {user}; password = {password}";
